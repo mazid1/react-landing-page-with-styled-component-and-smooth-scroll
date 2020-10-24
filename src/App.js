3f513abeba8419b356524a4839d00ea173b186c1
mazid <mazidmailbox@gmail.com>
2020-10-24 19:11:43 +0600

    Router and signin page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />
+      </Switch>
     </Router>
   );
 }
