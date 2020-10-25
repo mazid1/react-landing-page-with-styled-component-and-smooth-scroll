5658c5561521848c9d71347a7f5a193ef65736a0
mazid <mazidmailbox@gmail.com>
2020-10-25 11:34:55 +0600

    Updates

---
+++ precache-manifest.409010241b7ab63b207fa5d4e280aac2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69e28bcff566f4da93a5c63065d91a5",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/index.html"
+  },
+  {
+    "revision": "cc9dfdd4cd958729d616",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/css/main.4c1d1e7e.chunk.css"
+  },
+  {
+    "revision": "40513d6c047ee6e3adfb",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/js/2.bc995d47.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/js/2.bc995d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9dfdd4cd958729d616",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/js/main.859713ec.chunk.js"
+  },
+  {
+    "revision": "8c8f7b3feed29d86466f",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/js/runtime-main.4ad6047d.js"
+  },
+  {
+    "revision": "50b81502344e636988522d9990379c4e",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-1.50b81502.svg"
+  },
+  {
+    "revision": "48a0ae84b990057c49333715482535df",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-2.48a0ae84.svg"
+  },
+  {
+    "revision": "c1c40e5c87179577393440f6b0b211b6",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-3.c1c40e5c.svg"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-4.715313cd.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-5.d9e440a5.svg"
+  },
+  {
+    "revision": "1dbb6f109f64dd591c9d154e757736e1",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/svg-6.1dbb6f10.svg"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/mazid1/react-landing-page-with-styled-component-and-smooth-scroll/static/media/video.ed44dc5d.mp4"
+  }
+]);
