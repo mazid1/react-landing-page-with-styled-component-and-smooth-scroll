2ae7e8a87d2f3b59b50f1081f29a9a3bdd80dfee
mazid <mazidmailbox@gmail.com>
2020-10-25 20:06:58 +0600

    published to github-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages';
 import SigninPage from './pages/signin';
 
